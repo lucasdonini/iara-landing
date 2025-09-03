28fa00d5e835df4910f02202cd0038825401f291
ryancursinom <rcursinomoraes@gmail.com>
2025-09-03 09:36:11 -0300

    Arrumando a classe Planos, uma vez que a classe AbstractModel foi removida

--- src/main/java/com/model/Planos.java
+++ src/main/java/com/model/Planos.java
@@ -1,23 +1,35 @@
 package com.model;
 
-public class PlanosModel extends AbstractModel {
+public class Planos{
 //    atributos
+    private int id;
+    private String nome;
     private double valor;
     private String descricao;
 
 //    construtor
-    public PlanosModel(int id, String nome, double valor, String descricao){
-        super(id, nome);
+    public Planos(int id, String nome, double valor, String descricao){
+        this.id = id;
+        this.nome = nome;
         this.valor = valor;
         this.descricao = descricao;
     }
 
 //    toString
     public String toString(){
-        return String.format("%sValor: R$%.2f\nDescrição: %s\n", super.toString(), this.valor, this.descricao);
+        return String.format("ID: %d\nNome: %s\nValor: R$%.2f\nDescrição: %s\n", this.id, this.nome, this.valor, this.descricao);
     }
 
 //    getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
     public double getValor() {
         return valor;
     }
