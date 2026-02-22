9663498ad1cd5d11a7879731db3a533b0b1b2757
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-02-22 18:40:34 -0300

    feat(database keep-alive): Implement python keep-alive script through github actions

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+
+def poke():
+    try:
+        with psycopg2.connect(os.getenv('DATABASE_URL')) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('SELECT 1')
+
+    except (Exception, psycopg2.Error) as error:
+        print(f'Failed to connect: {error}')
+
+
+load_dotenv()
+poke()
